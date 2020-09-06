b12e8fe44227a3d0cce83edf61ea9ec0ac2a433e
Phonedolly <Phonedolly@users.noreply.github.com>
2020-09-06 19:28:04 +0900

    모바일 뉴스 페이지에서 크롤링 구현

--- crawler.py
+++ crawler.py
@@ -1,14 +1,23 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
-# ct > div.media_end_head.go_trans > div.media_end_head_title > h2
+def crawler(ChromeDriverDir):
+    newsUrl = input("뉴스의 URL을 입력하세요 : ")
+    driver = webdriver.Chrome(ChromeDriverDir)
+    driver.implicitly_wait(3)  # 암묵적으로 웹 자원 로드를 위해 3초까지 기다려준다
+    print("webdriver 조작중...")
+    print("url 접근")
+    driver.get(newsUrl)  # url에 접근한다
+    print("html객체 파싱중..")
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+    print("webdriver 종료")
 
-def crawler():
-    req = requests.get('https://n.news.naver.com/article/001/0011862376?cds=news_edit')
-    html = req.text
-    soup = BeautifulSoup(req.text, 'html.parser')  # python이 인식할 수 있는 html 객체로 변환
-
+    print("컨텐츠 추출중...")
     journalName = soup.select_one(
         '#contents > div.media_end_linked_more > div > a > em'
     ).text  # 언론사 이름
@@ -16,26 +25,23 @@ def crawler():
         '#ct > div.media_end_head.go_trans > div.media_end_head_title > h2'
     ).text  # 기사 제목
 
-    # TODO 기사 내용 불러오기 (Selenium 써보기)
-    contextList = soup.select(
+    # 기사 내용 (.arcitle_p로 분리되어있는 기사 문단들의 배열)
+    articleElementList = soup.select(
         '#dic_area > span.article_p'
     )
-    # dic_area > span:nth-child(40)
-    # dic_area > span:nth-child(3)
-    # dic_area > span:nth-child(11)
 
-    context = []
-    for i in range(0, 2):
-        context[i] = contextList[i].text
-
-    # TODO 기사 요약봇 내용 불러오기
-
-    saver(journalName, title, context)
+    # articleElementList에서 기사를 수집해 article에 저장
+    article = ""
+    for articleElement in articleElementList:
+        article += articleElement.text.strip() + os.linesep  # articleElement.text에는 맨 앞에 4단위 whitespace가 존재한다. 이를 strip()으로 제거해준다.
+    # 마지막 공백 제거
+    article = article.rstrip()
+    saver(journalName, title, article)
 
 
-#  #contents > div.media_end_linked_more > div > a > em
-def saver(journalName, title, context):
+def saver(journalName, title, article):
+    print("저장중...")
     with open("dump.news", 'w', encoding='utf-8') as f:
-        f.write(journalName)
-        f.write(title)
-        f.write(context)
+        f.write("신문사 : " + journalName + '\n')
+        f.write("제목 : " + title + '\n\n')
+        f.write(article)
--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 # news2atc
 # initially created by Phonedolly at 2020-09-05
+import os
+import platform
+import json
 
 import crawler
 
+OS_TYPE = platform.system()
+
+
+def clearScreen():
+    if OS_TYPE != 'Windows':
+        os.system('clear')  # not windows
+    else:
+        os.system('cls')  # windows
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Hello! news2atc")
-    crawler.crawler()
+    try:
+        with open('config.json') as f:
+            json_data = json.load(f)
+            ChromeDriverDir = json_data['ChromeDriverDir']
+            # PhantomJSDir = json_data['PhantomJSDir']
+    except KeyError:
+        print("config.json의 형식이 올바르지 않습니다. config.json은 'ChromeDriverDir' 키와 적절한 값(chromedriver의 경로)을 포함해야합니다.")
+        exit(1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while (True):
+        clearScreen()
+        print("=============Hello! news2atc=============")
+        print("c. 뉴스 가져오기")
+        print("q. 끝내기")
+        print("h. 도움말")
+
+        option = input("\n\n옵션을 입력하세요 : ")
+
+        if option == 'c':
+            crawler.crawler(ChromeDriverDir)
+        elif option == 'q':
+            print("bye <3")
+            break;
+        elif option == 'h':
+            print("사용해주셔서 감사합니다! 이 프로그램의 소스 코드는 https://github.com/Phonedolly/news2atc에서 확인하실 수 있습니다.\n\n계속하려면 Enter",
+                  end="\n\n")
+            input()
