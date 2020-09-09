f50a0248904e400df5f2cc15f1116f4d5c22c479
Phonedolly <Phonedolly@users.noreply.github.com>
2020-09-10 04:06:21 +0900

    훈련병 찾기 버튼이 작동안되는 문제 해결
    며칠 전엔 주석처리한 코드가 작동했었는데 이번에는 아니었다

--- sender.py
+++ sender.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 def sender(recruitData, articleData, ChromeDriverDir):
+    # TODO 예외처리하기
+
     atcURL = "http://www.katc.mil.kr/katc/community/children.jsp"
 
     print("육군훈련소에 뉴스를 보냅니다.")
@@ -26,8 +28,9 @@ def sender(recruitData, articleData, ChromeDriverDir):
     driver.find_element_by_id('search_val3').send_keys(recruitData['name'])
 
     # 찾기 버튼 클릭
-    driver.find_element_by_xpath(
-        '//*[@id="item_body"]/div[2]/div/div/div[2]/div[3]/div/div/div[2]/form/fieldset/input[7]').click()
+    # driver.find_element_by_xpath(
+    #     '//*[@id="item_body"]/div[2]/div/div/div[2]/div[3]/div/div/div[2]/form/fieldset/input[7]').click()
+    driver.find_element_by_xpath('//*[@id="item_body"]/div[2]/div/div/div[2]/div[3]/div/div/div[2]/form/fieldset/input[7]').send_keys(Keys.ENTER)
 
     # 해당 장병 선택
     # TODO 같은 조건의 장병이 있을 때 선택하도록 하기
